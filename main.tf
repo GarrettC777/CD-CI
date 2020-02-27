c584fd0655fb7e041283934c4ac7719205f4a4a4
GarrettC777 <garrett.church.820@my.csun.edu>
2020-02-26 20:18:12 -0800

    added provider, as well as s3 configuration.

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "thegroupdotbucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_codecommit_repository" "test" {
   repository_name = "MyTestRepository"
   description     = "This is the Sample App Repository"
