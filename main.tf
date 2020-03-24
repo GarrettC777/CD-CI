09977a97d6031fb7f68223a4f59a9e21fe7fb507
GarrettC777 <garrett.church.820@my.csun.edu>
2020-03-24 16:22:15 -0700

    Update

--- main.tf
+++ main.tf
@@ -29,3 +29,51 @@ resource "aws_codecommit_repository" "test" {
   repository_name = "MyTestRepository"
   description     = "This is the Sample App Repository"
 }
+
+resource "aws_iam_group" "thegroup" {
+  name   = "thegroup"
+  path   = "/cit480/"
+}
+
+resource "aws_iam_user" "user_creation" {
+  count  = "${length(var.username)}"
+  name   = "${element(var.username, count.index)}"
+  path   = "/cit480/"
+}
+
+resource "aws_iam_user_group_membership" "add" {
+  count = "${length(var.username)}"
+  user = "${element(var.username, count.index)}"
+
+  groups = [
+    "${aws_iam_group.thegroup.name}",
+  ]
+}
+
+resource "aws_iam_group_policy" "cit480policy" {
+  name = "cit480policy"
+  group = "${aws_iam_group.thegroup.id}"
+  
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+
+ resource "aws_iam_user_login_profile" "loginprofile" {
+  count  = "${length(var.username)}"
+  user = "${element(var.username, count.index)}"
+  pgp_key = "${element(var.keybase, count.index)}"
+}
+
+output "password" {
+  value = "${aws_iam_user_login_profile.loginprofile.*.encrypted_password}"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+  type    = "list"
+  default = ["mark", "artur", "luis", "gary"]
+}
+
+variable "keybase" {
+  type    = "list"
+  default = ["keybase:MLawton", "keybase:atarverdyan", "keybase:melendez2294", "keybase:garrettc777"]
+}
