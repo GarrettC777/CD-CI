d9ae14782fbac3c9a2fe9130a52f24d9f727501f
GarrettC777 <garrett.church.820@my.csun.edu>
2020-03-26 20:26:58 -0700

    updated VPC and Routing Info

--- Codepipeline/main/main.tf
+++ Codepipeline/main/main.tf
@@ -22,9 +22,63 @@ resource "aws_codecommit_repository" "test" {
   description     = "This is the Sample App Repository"
 }
 
+# Define the VPC
+resource "aws_vpc" "the_main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
+  tags {
+    Name = "main-vpc"
+  }
+}
 
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.the_main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2"
 
+  tags {
+    Name = "public subnet"
+  }
+}
 
+# Define the private subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = "${aws_vpc.the_main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2"
 
+  tags {
+    Name = "private subnet"
+  }
+}
 
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.the_main.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.public-rt.id}"
+}
