283157ea2de9664375276b3244db31a49ad2583a
GarrettC777 <garrett.church.820@my.csun.edu>
2020-03-26 19:29:42 -0700

    Update

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "username" {
-  type    = "list"
-  default = ["mark", "artur", "luis", "gary"]
-}
-
-variable "keybase" {
-  type    = "list"
-  default = ["keybase:MLawton", "keybase:atarverdyan", "keybase:melendez2294", "keybase:garrettc777"]
-}
