7f3d3353dde2e3a994b7f58a61c801efe538913a
GarrettC777 <garrett.church.820@my.csun.edu>
2020-04-10 16:26:22 -0700

    Added associate_public_ip_address

--- Codepipeline/main/EC2.tf
+++ Codepipeline/main/EC2.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "bastion" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   subnet_id = "${aws_subnet.public-subnet.id}"
-
+  key_name = "beef"
+  associate_public_ip_address = true
+  
   tags = {
     Name = "Bastion Host"
   }
