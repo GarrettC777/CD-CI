fcb9b4f059977506f69e9c0a0a617653605d16fc
GarrettC777 <garrett.church.820@my.csun.edu>
2020-04-27 19:35:04 -0700

    added ELB

--- Codepipeline/main/main.tf
+++ Codepipeline/main/main.tf
@@ -31,25 +31,47 @@ resource "aws_vpc" "the_main" {
   }
 }
 
-# Define the public subnet
-resource "aws_subnet" "public-subnet" {
+# Define the public subnet 1
+resource "aws_subnet" "public-subnet-1" {
   vpc_id            = "${aws_vpc.the_main.id}"
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "public subnet"
+    Name = "public subnet 1"
   }
 }
 
-# Define the private subnet
-resource "aws_subnet" "private-subnet" {
+# Define the public subnet 2
+resource "aws_subnet" "public-subnet-2" {
   vpc_id            = "${aws_vpc.the_main.id}"
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-west-2b"
 
   tags = {
-    Name = "private subnet"
+    Name = "public subnet 2"
+  }
+}
+
+# Define the private subnet 1
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id            = "${aws_vpc.the_main.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "private subnet 1"
+  }
+}
+
+# Define the private subnet 2
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id            = "${aws_vpc.the_main.id}"
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "private subnet 2"
   }
 }
 
@@ -65,7 +87,7 @@ resource "aws_internet_gateway" "igw" {
 # Define NAT Gateway
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.public-subnet.id}"
+  subnet_id     = "${aws_subnet.public-subnet-1.id}"
 
   tags = {
   Name = "gw NAT"
@@ -100,15 +122,27 @@ resource "aws_route_table" "private-rt" {
   }
 }
 
-# Assign the public route table to the public Subnet
-resource "aws_route_table_association" "public-rt-assoc" {
-  subnet_id      = "${aws_subnet.public-subnet.id}"
+# Assign the public route table to the public Subnet 1
+resource "aws_route_table_association" "public-rt-assoc-1" {
+  subnet_id      = "${aws_subnet.public-subnet-1.id}"
   route_table_id = "${aws_route_table.public-rt.id}"
 }
 
-# Assign the private route table to the private Subnet
-resource "aws_route_table_association" "private-rt-assoc" {
-  subnet_id      = "${aws_subnet.private-subnet.id}"
+# Assign the public route table to the public Subnet 2
+resource "aws_route_table_association" "public-rt-assoc-2" {
+  subnet_id      = "${aws_subnet.public-subnet-2.id}"
+  route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+# Assign the private route table to the private Subnet 1
+resource "aws_route_table_association" "private-rt-assoc-1" {
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
+  route_table_id = "${aws_route_table.private-rt.id}"
+}
+
+# Assign the private route table to the private Subnet 2
+resource "aws_route_table_association" "private-rt-assoc-2" {
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
   route_table_id = "${aws_route_table.private-rt.id}"
 }
 
@@ -116,4 +150,43 @@ resource "aws_route_table_association" "private-rt-assoc" {
 resource "aws_eip" "nat" {
   vpc      = true
    depends_on = ["aws_internet_gateway.igw"]
-} 
+}
+
+# Create a new load balancer
+resource "aws_elb" "elb" {
+  name               = "vpc-elb"
+  #availability_zones = ["us-west-2a","us-west-2b"]
+  subnets = ["${aws_subnet.public-subnet-1.id}", "${aws_subnet.public-subnet-2.id}"]
+  security_groups = ["${aws_security_group.allow_http.id}"]
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "discource-elb"
+  }
+}
+
+resource "aws_elb_attachment" "yup" {
+  elb      = "${aws_elb.elb.id}"
+  instance = "${aws_instance.webserver.id}"
+
+  depends_on = ["aws_elb.elb"]
+}
